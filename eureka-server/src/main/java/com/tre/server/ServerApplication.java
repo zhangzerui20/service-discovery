2ecc7c1d03fd8f98a5e2255ae7d35281a8188201
trezhang <trezhang@tencent.com>
2020-01-21 00:49:10 +0800

    更新了一些注释

--- eureka-server/src/main/java/com/tre/server/ServerApplication.java
+++ eureka-server/src/main/java/com/tre/server/ServerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+
+/**
+ * @author trezhang
+ */
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
